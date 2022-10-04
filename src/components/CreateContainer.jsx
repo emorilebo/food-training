616c6a9f406dbd478b62607ef01f11f3a5365f7f
Godfrey Lebo <emorylebo@gmail.com>
2022-10-04 22:59:22 +0100

    moving forward

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const CreateContainer = () => {
-  return <div>Create</div>;
+  return (
+    <div>
+      <div>This is another screen</div>
+    </div>
+  );
 };
 
 export default CreateContainer;
