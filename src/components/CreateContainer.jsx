1d1f99543d4272bfe89cfd1d7b5cd38631548548
Godfrey Lebo <emorylebo@gmail.com>
2022-10-04 23:47:42 +0100

    moving forward.

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -3,8 +3,7 @@ import React from "react";
 const CreateContainer = () => {
   return (
     <div>
-      <div>This is another screen ywag
-      </div>
+      <div>This is another screen</div>
     </div>
   );
 };
