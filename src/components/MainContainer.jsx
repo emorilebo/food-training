7e9fcfc9163337b6752d46b1b4fb0d4d1724fe7b
Godfrey Lebo <emorylebo@gmail.com>
2022-10-08 21:24:41 +0100

    UPDATE Maincontainer with bike delivery

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from "react";
+import Delivery from "../img/delivery.png";
 
 const MainContainer = () => {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2'>
-      <div className='p-4 bg-blue-400 flex-1'>Main</div>
-      <div className='p-4 bg-blue-400 flex-1'>Min</div>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+      <div className="py-2 flex-1 flex flex-col items-start md:items-center justify-center">
+        <div className="flex items-center justify-center bg-orange-100 p-2">
+          <p>Bike Delivery</p>
+          <div className="w-8 h-8 bg-white rounded-full overflow-hidden">
+            <img className="w-full h-full object-contain" src={Delivery} alt="delivery" />
+          </div>
+        </div>
+      </div>
+      <div className="py-2 bg-blue-400 flex-1"></div>
     </div>
-  )
-}
+  );
+};
 
-export default MainContainer
+export default MainContainer;
